1434c01ab5820a047aaaf0a3db4e4a58b6599a5e
John Bussey <jbussey365@yahoo.com>
2015-05-24 23:14:31 -0400

    put csrf filter on base controller. Resolves issue #5

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,23 @@
 
 class BaseController extends Controller {
 
+	public $breadcrumbs = array();
+
+  /**
+   * Initializer.
+   *
+   * @access   public
+   * @return \BaseController
+   */
+    public function __construct()
+    {
+        $this->beforeFilter('csrf', array('on' => array('post', 'delete', 'put')));
+        $this->beforeFilter('ajax', array('on' => array('post', 'delete', 'put')));
+        $this->setNav('');
+        View::share('breadcrumbs', $this->breadcrumbs);
+    }
+
+
 	/**
 	 * Setup the layout used by the controller.
 	 *
@@ -14,5 +31,31 @@ class BaseController extends Controller {
 			$this->layout = View::make($this->layout);
 		}
 	}
+
+    protected function setNav($nav)
+    {
+        View::share('current_nav', $nav);
+    }
+
+	protected function addCrumb($name, $url='')
+	{
+        if($name){
+    		$this->breadcrumbs[] = array(
+     			'name' => $name,
+     			'url' => $url
+     		);
+        }
+
+        View::share('breadcrumbs', $this->breadcrumbs);
+ 		
+        return $this->breadcrumbs;
+    }
+
+    protected function resetCrumbs($name=null, $url=null)
+    {
+        $this->breadcrumbs = array();
+
+        return $this->addCrumb($name, $url='');
+    }
 
 }
